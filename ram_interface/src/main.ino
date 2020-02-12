8806d3db0f2962bb90405a5a0716cb74b8fb1ec0
Henrik <hhs19942@gmail.com>
2020-02-12 09:52:47 +0100

    Add eeprom, button and hlt

--- ram_interface/src/main.ino
+++ ram_interface/src/main.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
+#include <EEPROM.h>
 
-#define DELAY 5
+#define DELAY 10
 
 #define BUS0_PIN 2
 #define BUS1_PIN 3
@@ -10,13 +11,24 @@
 #define BUS5_PIN 7
 #define BUS6_PIN 8
 #define BUS7_PIN 9
+
 #define MI_PIN 10
 #define RI_PIN 11
 #define CLK_PIN 12
 #define OE_PIN 13
+#define HLT_PIN 15
+
+#define BTN_PIN 14
+
+long lastButtonPress = 0;
 
 void setup() {
   disablePins();
+  pinMode(BTN_PIN, INPUT);
+  digitalWrite(BTN_PIN, HIGH);
+
+  pinMode(CLK_PIN, OUTPUT);
+  digitalWrite(OE_PIN, LOW);
 
   Serial.begin(115200);
   Serial.setTimeout(30000);
@@ -30,6 +42,17 @@ void loop() {
       uint8_t addr = parseBinary(line.substring(0, 4));
       uint8_t data = parseBinary(line.substring(5, 13));
       writeRAM(addr, data);
+      EEPROM.write(addr, data);
+    }
+  }
+
+  if (!digitalRead(BTN_PIN)) {
+    long now = millis();
+    if (now - lastButtonPress > 1000) {
+      lastButtonPress = now;
+      for (int i = 0; i < 16; ++i) {
+        writeRAM(i, EEPROM.read(i));
+      }
     }
   }
 }
@@ -47,15 +70,16 @@ uint8_t parseBinary(String str) {
 }
 
 void enablePins() {
+  pinMode(OE_PIN, OUTPUT);  
+  pinMode(HLT_PIN, OUTPUT);
+  digitalWrite(OE_PIN, HIGH);
+  digitalWrite(HLT_PIN, HIGH);
+  delay(DELAY);
+
   for (uint8_t pin = BUS0_PIN; pin <= RI_PIN; pin++) {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
-  }
-
-  pinMode(CLK_PIN, OUTPUT);
-  pinMode(OE_PIN, OUTPUT);
-  digitalWrite(CLK_PIN, LOW);
-  digitalWrite(OE_PIN, HIGH);
+  } 
 }
 
 void disablePins() {
@@ -64,34 +88,25 @@ void disablePins() {
     digitalWrite(pin, LOW);
   }
 
-  pinMode(CLK_PIN, OUTPUT);
+  pinMode(HLT_PIN, INPUT);
   pinMode(OE_PIN, OUTPUT);
-  digitalWrite(CLK_PIN, LOW);
-  digitalWrite(OE_PIN, LOW); 
+  digitalWrite(HLT_PIN, LOW);
+  digitalWrite(OE_PIN, LOW);
+  delay(DELAY);
 }
 
-void pulseClk() {
-  digitalWrite(CLK_PIN, HIGH);
+void pulse(uint8_t pin) {
+  digitalWrite(pin, HIGH);
   delay(DELAY);
-  digitalWrite(CLK_PIN, LOW);
-}
-
-void pulseMI() {
-  digitalWrite(MI_PIN, HIGH);
-  delay(DELAY);
-  pulseClk();
-  digitalWrite(MI_PIN, LOW);
-}
-
-void pulseRI() {
-  digitalWrite(RI_PIN, HIGH);
-  delay(DELAY);
-  pulseClk();
-  digitalWrite(RI_PIN, LOW);
+  if (pin != CLK_PIN) {
+    pulse(CLK_PIN);
+  }
+  digitalWrite(pin, LOW);
 }
 
 void writeRAM(uint8_t addr, uint8_t data) {
-  enablePins(); 
+  enablePins();
+  delay(DELAY);
 
   // Write address to bus
   for (uint8_t busPin = BUS0_PIN; busPin <= BUS3_PIN; busPin++) {
@@ -100,7 +115,7 @@ void writeRAM(uint8_t addr, uint8_t data) {
   }
 
   // Write bus to address register
-  pulseMI();
+  pulse(MI_PIN);
 
   // Write data to bus
   for (uint8_t busPin = BUS0_PIN; busPin <= BUS7_PIN; busPin++) {
@@ -109,7 +124,7 @@ void writeRAM(uint8_t addr, uint8_t data) {
   }
 
   // Write bus to RAM
-  pulseRI();
+  pulse(RI_PIN);
 
   disablePins();
   delay(DELAY);
