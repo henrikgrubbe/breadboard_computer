0f3cc34d1d299000bb928e5daff3714df6665b18
Henrik <hhs19942@gmail.com>
2020-02-12 09:53:10 +0100

    set dtr false

--- compiler/transmitter.py
+++ compiler/transmitter.py
@@ -3,7 +3,11 @@ import serial as sl
 import fileinput
 import time
 
-ser = sl.Serial('/dev/ttyUSB0',115200)
+ser = sl.Serial()
+ser.dtr = False
+ser.port = '/dev/ttyUSB0'
+ser.baudrate = 115200
+ser.open()
 
 time.sleep(2)
 ser.write("\n".encode('utf-8'))
