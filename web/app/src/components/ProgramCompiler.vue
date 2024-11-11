eacbe9e420da10ea96ae5c4c725041115fbf73db
Henrik Herbst Grubbe <hhb@bankdata.dk>
2024-11-11 09:54:33 +0100

    Update app to use Vite

--- web/app/src/components/ProgramCompiler.vue
+++ web/app/src/components/ProgramCompiler.vue
@@ -2,10 +2,12 @@
   <div class="row mb-5">
     <div class="col-12 col-lg-6 mb-3 overflow-hidden">
       <label class="form-label">Write your program</label>
-      <prism-editor class="editor source"
-                    v-model="source"
-                    :highlight="sourceHighlighter"
-                    :line-numbers="true">
+      <prism-editor
+        class="editor source"
+        v-model="source"
+        :highlight="sourceHighlighter"
+        :line-numbers="true"
+      >
       </prism-editor>
     </div>
 
@@ -13,26 +15,43 @@
       <div class="mb-3">
         <label for="program-txt" class="form-label">Compiled program</label>
         <div v-if="error">
-          <prism-editor class="editor error"
-                        v-model="error"
-                        :highlight="(err) => err"
-                        :readonly="true">
+          <prism-editor
+            class="editor error"
+            v-model="error"
+            :highlight="(err: unknown) => err"
+            :readonly="true"
+          >
           </prism-editor>
         </div>
         <div v-else>
-          <prism-editor class="editor compiled"
-                        v-model="compiled"
-                        :highlight="compiledHighlighter"
-                        :readonly="true">
+          <prism-editor
+            class="editor compiled"
+            v-model="compiled"
+            :highlight="compiledHighlighter"
+            :readonly="true"
+          >
           </prism-editor>
         </div>
       </div>
 
-      <div class="btn-group d-flex justify-content-end" role="group" aria-label="button group">
-        <button @click="uploadProgram" :disabled="error !== ''" type="button" class="btn btn-lg btn-primary">
+      <div
+        class="btn-group d-flex justify-content-end"
+        role="group"
+        aria-label="button group"
+      >
+        <button
+          @click="uploadProgram"
+          :disabled="error !== ''"
+          type="button"
+          class="btn btn-lg btn-primary"
+        >
           Upload
         </button>
-        <button @click="writeProgram" type="button" class="btn btn-lg btn-success">
+        <button
+          @click="writeProgram"
+          type="button"
+          class="btn btn-lg btn-success"
+        >
           Write to RAM
         </button>
       </div>
@@ -41,31 +60,36 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
-import {PrismEditor} from 'vue-prism-editor';
-import * as peggy from 'peggy';
-import {debounceFunction} from '@/utils/debounceFunction';
-import {publishToTopic} from '@/utils/mqtt-upload';
-import 'vue-prism-editor/dist/prismeditor.min.css';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const grammar = require('@/assets/grammar.peggy?raw');
+import { defineComponent } from "vue";
+import { PrismEditor } from "vue-prism-editor";
+import * as peggy from "peggy";
+import { debounceFunction } from "@/utils/debounceFunction";
+import { publishToTopic } from "@/utils/mqtt-upload";
+import "vue-prism-editor/dist/prismeditor.min.css";
+// @ts-expect-error TS2307
+import * as grammar from "@/assets/grammar.peggy?raw";
+import type { GrammarInput } from "peggy";
 
 export default defineComponent({
-  name: 'ProgramCompiler',
+  name: "ProgramCompiler",
   components: {
-    PrismEditor
+    PrismEditor,
   },
   data() {
     return {
-      parser: peggy.generate(grammar),
-      source: '' as string,
-      compiled: '' as string,
-      error: ' ' as string,
-      debouncedCompiler: debounceFunction((code: string) => this.compile(code), 250),
+      parser: peggy.generate(grammar.default as GrammarInput),
+      source: "" as string,
+      compiled: "" as string,
+      error: " " as string,
+      debouncedCompiler: debounceFunction(
+        (code: string) => this.compile(code),
+        250,
+      ),
     };
   },
   mounted() {
-    this.source = '      ldi 4\nx:j:  add data\n      out\n      jmp j\ndata: 0x02\n'
+    this.source =
+      "      ldi 4\nx:j:  add data\n      out\n      jmp j\ndata: 0x02\n";
   },
   watch: {
     source: function (code: string) {
@@ -76,22 +100,27 @@ export default defineComponent({
     compile(code: string): void {
       let parsed: string;
       try {
-        parsed = this.parser.parse(code, {compile: true});
+        parsed = this.parser.parse(code, { compile: true });
       } catch (e: unknown) {
         this.error = (e as { message: string }).message as string;
         return;
       }
-      this.error = '';
+      this.error = "";
       this.compiled = parsed;
     },
     sourceHighlighter(code: string) {
-      return this.parser.parse(code, {highlight: true});
+      return this.parser.parse(code, { highlight: true });
     },
     compiledHighlighter(code: string) {
       return code
-          .split('\n')
-          .map((line: string) => line.replace(/(\d{4}):(\d{4})(\d{4})/, '<addr>$1</addr>:<instr>$2</instr><opcode>$3</opcode>'))
-          .join('\n');
+        .split("\n")
+        .map((line: string) =>
+          line.replace(
+            /(\d{4}):(\d{4})(\d{4})/,
+            "<addr>$1</addr>:<instr>$2</instr><opcode>$3</opcode>",
+          ),
+        )
+        .join("\n");
     },
     uploadProgram(): void {
       if (this.error) {
@@ -99,33 +128,38 @@ export default defineComponent({
       }
 
       const body: Record<string, number> = {};
-      this.compiled.split('\n')
-          .forEach((line: string) => {
-            const [address, instruction] = line.split(':');
-            body[parseInt(address, 2).toString()] = parseInt(instruction, 2);
-          });
+      this.compiled.split("\n").forEach((line: string) => {
+        const [address, instruction] = line.split(":");
+        body[parseInt(address, 2).toString()] = parseInt(instruction, 2);
+      });
 
-      publishToTopic('program', body);
+      publishToTopic("program", body);
     },
     writeProgram(): void {
-      publishToTopic('command', {
-        command: 'PROGRAM',
+      publishToTopic("command", {
+        command: "PROGRAM",
       });
     },
-  }
+  },
 });
 </script>
 
 <style lang="scss">
 .editor {
-  background: #2B2B2B;
-  color: #A9B7C6;
+  background: #2b2b2b;
+  color: #a9b7c6;
   padding: 5px;
 }
 
 .editor,
 .prism-editor__line-number {
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-family:
+    Fira code,
+    Fira Mono,
+    Consolas,
+    Menlo,
+    Courier,
+    monospace;
   font-size: 1rem;
   line-height: 1.5;
 }
@@ -134,27 +168,26 @@ export default defineComponent({
   outline: none;
 }
 
-
 .prism-editor-wrapper.source {
   token-mnemonic {
-    color: #CC7832;
+    color: #cc7832;
   }
 
   token-immediate {
-    color: #6897BB;
+    color: #6897bb;
   }
 
   token-instr token-label {
-    color: #9876AA;
+    color: #9876aa;
 
-    &[defined='false'] {
-      color: #BC3F3C;
+    &[defined="false"] {
+      color: #bc3f3c;
 
       display: inline-block;
       position: relative;
 
       &:after {
-        content: '';
+        content: "";
         width: 100%;
         border-bottom: 2px dotted red;
         position: absolute;
@@ -172,25 +205,25 @@ export default defineComponent({
   }
 
   token-label-definition token-label {
-    color: #FFC66D;
+    color: #ffc66d;
   }
 }
 
 .prism-editor-wrapper.error {
-  color: #BC3F3C;
+  color: #bc3f3c;
 }
 
 .prism-editor-wrapper.compiled {
   addr {
-    color: #A5C261;
+    color: #a5c261;
   }
 
   instr {
-    color: #6D9CBE;
+    color: #6d9cbe;
   }
 
   opcode {
-    color: #BC3F3C;
+    color: #bc3f3c;
   }
 }
 </style>
--- web/app/src/utils/mqtt-upload.ts
+++ web/app/src/utils/mqtt-upload.ts
@@ -1,14 +1,14 @@
-const HOST = '.';
+const HOST = ".";
 
 export function publishToTopic(topic: string, data: object): Promise<void> {
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    };
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
 
-    return fetch(`${HOST}/publish/${topic}`, requestOptions)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch(error => console.error('There was an error!', error));
+  return fetch(`${HOST}/publish/${topic}`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("There was an error!", error));
 }
